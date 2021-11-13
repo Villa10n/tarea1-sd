22a47e7ae2c68a5c74d8e93106b949eeb2f1615e
JoseArteaga <62229012+JoseArteaga@users.noreply.github.com>
2021-11-13 17:05:22 -0300

    Update rutas.js

--- routes/rutas.js
+++ routes/rutas.js
@@ -21,12 +21,43 @@ router.post('/orders', async (req, res) => {
     });
 });
 
+router.get('/DailySummary2', async (req, res) => {
+    const client = new kafka.KafkaClient({kafkaHost: 'localhost:9092'});
+    var consumer = new kafka.Consumer(client, [ { topic: 'orders' } ]);
+    await consumer.on('message', function (message) {
+    	console.log(message);
+	});
+    consumer.commit(function(err, data) {
+        return res.status(200).json({
+            ok: true,
+            msg: 'daily'
+        });
+    });
+    // Desconectemos al consumidor
+});
+
 router.get('/DailySummary', async (req, res) => {
     const client = new kafka.KafkaClient({kafkaHost: 'localhost:9092'});
     var consumer = new kafka.Consumer(client, [ { topic: 'orders' } ]);
     await consumer.on('message', function (message) {
     	console.log(message);
 	});
+    arr=[]
+    contador=0
+    //Escucha solo por 10 segundos y corta la coxion, cuando deja de escucha envia un mensaje
+    dia=setInterval(function() {
+        consumer.on('message', function (message) {
+            console.log(message);
+            arr.append(message);
+        });
+        contador=contador+1
+    }, 5000);
+
+    if (contador== 1){
+        clearInterval(dia)
+        print(arr)
+    }
+
     consumer.commit(function(err, data) {
         return res.status(200).json({
             ok: true,
