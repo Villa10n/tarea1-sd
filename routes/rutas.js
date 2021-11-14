a92a7ae0770e88dc10e0998cdc8d2d503eec5d50
JoseArteaga <62229012+JoseArteaga@users.noreply.github.com>
2021-11-13 20:08:25 -0600

    Update rutas.js

--- routes/rutas.js
+++ routes/rutas.js
@@ -46,6 +46,56 @@ router.get('/DailySummary', async (req, res) => {
 
     setTimeout(async function() {
         await consumer.disconnect();
+        var hoy=new Date();   
+        arr.forEach(element =>{
+            ids_carritos.push(
+                {
+                    'id':element["id_carrito"],
+                    'cocinero':element.["email_cocinero"],
+                    'vendedor':element.["email_vendedor"]
+                });
+        } );
+        //guarda los valores unicos de cada carrito
+
+        ids_carritos.forEach(function (car) {
+            // The code within the following block runs only if the
+            // current car does NOT exist in the uniqueCars list
+            // - a.k.a. prevent duplicates
+            if (uniques.indexOf(car["id"]) === -1) {
+                // Since we now know we haven't seen this car before,
+                // copy it to the end of the uniqueCars list.
+                uniques.push({"id":car["id"],'cocinero':car["cocinero"],'vendedor':car["vendedor"]);
+            }
+        });
+        console.log(uniques);
+        console.log(arr);
+        for (let i of uniques){
+            contador=0;
+            for(let j of arr ){
+                //cuando el id carrito coincide con el j.id le suma la cantidad
+                if( i == j.["id_carrito"]){
+                    //
+                    contador=contador+j["cantidad"]
+                }
+            }
+            // ingresar total y dia de carrito para el dailySumari
+            uniques[i]['total_vendido']=contador
+            uniques[i]['fecha']=`${hoy.getFullYear()}-${hoy.getMonth() + 1}-${hoy.getDate()}`
+        }
+        // Conexiones
+        const producer = kafka.producer();
+
+
+        // Guardamos los dailysummary en el topic 
+        await producer.connect()
+        for (let i uniques){
+            await producer.send({
+                        topic: 'dailySummary',
+                        messages: [
+                            { value: data },
+                        ],});
+        }
+        await producer.disconnect()
         console.log(arr);
     }, 10000);
     ids_carritos=[]
